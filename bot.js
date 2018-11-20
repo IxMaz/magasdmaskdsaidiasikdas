c1aad6ae9111507b7874294f30ccf0a196c21023
juliandaw <33635418+juliandaw@users.noreply.github.com>
2018-11-20 18:01:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const myID = "470366007748198401";
 
 client.on('ready', () => {
   console.log(`[Magix] Logged in as ${client.user.tag}!`);      
-  client.user.setGame(`Nothing`,`https://www.twitch.tv/julianxdark`);
+  client.user.setGame(`Magix Server ..`,`https://www.twitch.tv/julianxdark`);
 });
 
  client.on('message', message => {//help msg
@@ -23,10 +23,6 @@ client.on('ready', () => {
         __**الاوامر الادارية :gear:**__
 
   ❖${prefix}** bc ** ==>**لارسال رسالة لكل الاعضاء**
-  ❖${prefix}** role <user> ** ==>**لاعطاء شخص رتبة**
-  ❖${prefix}** role all ** ==>**لاعطاء الكل الرتبة المحددة**
-  ❖${prefix}** role humans ** ==>**لاعطاء الاعضاء فقط**
-  ❖${prefix}** role bots ** ==>**لاعطاء البوتات فقط**
   ❖${prefix}** ban ** ==>**لحظر شخص من السيرفر**
   ❖${prefix}** mute ** ==>**لاسكات عضو في السيرفر**
   ❖${prefix}** unmute ** ==>**لفك الاسكات عن عضو في السيرفر**
@@ -274,56 +270,6 @@ message.channel.send("``لا تستطيع سحب "+ message.mentions.members.fir
 } else {
 message.react("❌")
  }}});
-
-
-client.on("message", message => {
-	if (message.author.bot) return;
-	var prefix = "-";
-	var args = message.content.split(' ').slice(1); 
-	var msg = message.content.toLowerCase();
-	if( !message.guild ) return;
-	if( !msg.startsWith( prefix + 'role' ) ) return;
-	if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(':no_entry: | You dont have **MANAGE_ROLES** Permission!');
-	if( msg.toLowerCase().startsWith( prefix + 'roleremove' ) ){
-		if( !args[0] ) return message.reply( '**:x: يرجى وضع الشخص المراد سحب منه الرتبة**' );
-		if( !args[1] ) return message.reply( '**:x: يرجى وضع الرتبة المراد سحبها من الشخص**' );
-		var role = msg.split(' ').slice(2).join(" ").toLowerCase(); 
-		var role1 = message.guild.roles.filter( r=>r.name.toLowerCase().indexOf(role)>-1 ).first(); 
-		if( !role1 ) return message.reply( '**:x: يرجى وضع الرتبة المراد سحبها من الشخص**' );if( message.mentions.members.first() ){
-			message.mentions.members.first().removeRole( role1 );
-			return message.reply('**:white_check_mark: [ '+role1.name+' ] رتبة [ '+args[0]+' ] تم سحب من **');
-		}
-		if( args[0].toLowerCase() == "all" ){
-			message.guild.members.forEach(m=>m.removeRole( role1 ))
-			return	message.reply('**:white_check_mark: [ '+role1.name+' ] تم سحب من الكل رتبة**');
-		} else if( args[0].toLowerCase() == "bots" ){
-			message.guild.members.filter(m=>m.user.bot).forEach(m=>m.removeRole(role1))
-			return	message.reply('**:white_check_mark: [ '+role1.name+' ] تم سحب من البوتات رتبة**');
-		} else if( args[0].toLowerCase() == "humans" ){
-			message.guild.members.filter(m=>!m.user.bot).forEach(m=>m.removeRole(role1))
-			return	message.reply('**:white_check_mark: [ '+role1.name+' ] تم سحب من البشريين رتبة**');
-		} 	
-	} else {
-		if( !args[0] ) return message.reply( '**:x: يرجى وضع الشخص المراد اعطائها الرتبة**' );
-		if( !args[1] ) return message.reply( '**:x: يرجى وضع الرتبة المراد اعطائها للشخص**' );
-		var role = msg.split(' ').slice(2).join(" ").toLowerCase(); 
-		var role1 = message.guild.roles.filter( r=>r.name.toLowerCase().indexOf(role)>-1 ).first(); 
-		if( !role1 ) return message.reply( '**:x: يرجى وضع الرتبة المراد اعطائها للشخص**' );if( message.mentions.members.first() ){
-			message.mentions.members.first().addRole( role1 );
-			return message.reply('**:white_check_mark: [ '+role1.name+' ] رتبة [ '+args[0]+' ] تم اعطاء **');
-		}
-		if( args[0].toLowerCase() == "all" ){
-			message.guild.members.forEach(m=>m.addRole( role1 ))
-			return	message.reply('**:white_check_mark: [ '+role1.name+' ] تم اعطاء الكل رتبة**');
-		} else if( args[0].toLowerCase() == "bots" ){
-			message.guild.members.filter(m=>m.user.bot).forEach(m=>m.addRole(role1))
-			return	message.reply('**:white_check_mark: [ '+role1.name+' ] تم اعطاء البوتات رتبة**');
-		} else if( args[0].toLowerCase() == "humans" ){
-			message.guild.members.filter(m=>!m.user.bot).forEach(m=>m.addRole(role1))
-			return	message.reply('**:white_check_mark: [ '+role1.name+' ] تم اعطاء البشريين رتبة**');
-		} 
-	} 
-});
 
 client.on('message', message => {
 var prefix = "-";
